37f5108d8850c203699f1d3b7055aab14c1982fb
Peter Chapman <pnchappy92@gmail.com>
2017-11-12 19:15:29 -0500

    more refactor

--- src/containers/todo_text_input/index.tsx
+++ src/containers/todo_text_input/index.tsx
@@ -11,12 +11,12 @@ type TodoInputProp = {
   onSubmit?: () => any;
 }
 
-export default ({ editing = false, isNew = false, placeholder, value = '', onBlur, onChange, onSubmit }: TodoInputProp) => (
+export default ({ editing = false, isNew = false, onSubmit, ...props }: TodoInputProp) => (
   <input
     type='text'
     className={classnames({ edit: editing, 'new-todo': isNew })}
     autoFocus
     onKeyDown={e => e.which === 13 && onSubmit()}
-    { ...{ value, placeholder, onBlur, onChange } }
+    { ...props }
   />
 );
